a0db1056ed854fb868daa00dd3a5130cdc4f8de2
Sourabhjoshi542 <88243616+Sourabhjoshi542@users.noreply.github.com>
2024-02-04 13:37:56 +0000

    done

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -23,11 +23,44 @@ if(true)
 
 console.log("outer a",a)     both a are different because of they are declare inside different blocks
 
+
+
 */
 
 
+function one()
+{
+    const userName="Sourabh"
+        function two()
+        { 
+          const couse="javascript"
+          console.log(`the user of this course is ${userName}`)
+        } 
+
+    two()                   // nested function  
+   // console.log(course)  //outside the scope or block not executed
+}
+
+one()
 
 
 
 
+
+console.log(addOne(5))  // it will execute 
+
+function addOne(num)   //basic function
+{
+    return num+1
+}
+
+
+//console.log(addTwo(5)) not execute because it is writtten before declaration of the expression
+
+const addTwo=function (num)    // called Expression but its basically a function 
+{
+    return num+2
+}
+
+console.log(addTwo(5))
 
