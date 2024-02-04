30b20020fbe0a2f6cfa5a831cdc967e64c8ef482
Sourabhjoshi542 <88243616+Sourabhjoshi542@users.noreply.github.com>
2024-02-04 11:09:55 +0000

    done

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,33 @@
+
+/*
+if(true)
+{
+    let a=10
+    const b=30
+    var c=30
+}
+
+console.log(a)
+console.log(b)
+console.log(c) //it is printes as 30 but print honi nhi chahiye kyuki c inside the if declare hai.
+
+
+
+let a=100
+
+if(true)
+{
+    let a=10
+    console.log("inner a",a)
+}
+
+console.log("outer a",a)     both a are different because of they are declare inside different blocks
+
+*/
+
+
+
+
+
+
+
