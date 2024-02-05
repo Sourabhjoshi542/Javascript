25a9529604757f0bf6470feae310d5d0c50825af
Sourabhjoshi542 <88243616+Sourabhjoshi542@users.noreply.github.com>
2024-02-05 07:33:30 +0000

    done

---
+++ 05_iterations/01_loops.js
@@ -0,0 +1,26 @@
+//for loop
+
+//for(let i=1;i<=10;i++)
+//{
+//  console.log("table of ",i);
+//  for(let j=1;j<=10;j++)
+//  console.log(`${i} * ${j} = ${i*j}`);
+//}
+
+//while loop
+
+let num=0
+while(num<=5)
+{
+console.log(num)
+num++
+}
+
+
+//do while loop
+
+let a=5
+do{
+    console.log(a)
+    a++
+}while(a>10);
