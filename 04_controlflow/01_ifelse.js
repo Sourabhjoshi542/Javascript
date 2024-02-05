89c03ecf675da803fd27d646d1a8d6dd41afa58b
Sourabhjoshi542 <88243616+Sourabhjoshi542@users.noreply.github.com>
2024-02-05 05:55:55 +0000

    container config done

---
+++ 04_controlflow/01_ifelse.js
@@ -0,0 +1,31 @@
+//if
+
+if(2===2)                //=== also compare the data type
+{
+    console.log("execute if one");
+}
+else{
+    console.log("execute else one");
+}
+
+
+const userloggedin=true
+const hasdebitcard=true
+const logingoogle=true
+const loginemail=false
+
+if(userloggedin && hasdebitcard)   //&& to check multiple conditions
+{
+    console.log("can shop")
+}
+
+
+if(logingoogle || loginemail)
+{
+    console.log("can log in ")
+}
+
+
+
+
+
