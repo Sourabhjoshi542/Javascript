176c5f9610e65c102a4d70c64f94c0eba3f9bc3c
Sourabhjoshi542 <88243616+Sourabhjoshi542@users.noreply.github.com>
2024-02-05 09:19:46 +0000

    done

---
+++ 05_iterations/03_foreach.js
@@ -0,0 +1,35 @@
+//foreach loop - mostly used loop in arrays
+//callback function - function that do not have the name used inside foreach loop
+
+const arr=["car","bike","ships"]
+
+// arr.forEach( function (item){       //1st method -callback function
+//    console.log(item)
+// } )
+
+// arr.forEach((item) => {       //arrow function
+//    console.log(item)
+// } )
+
+function show(item)
+{
+    console.log(item)
+}
+
+arr.forEach(show)   //passing simple function referenece
+
+
+let arrayOfObject=[{
+    name:"Sourabh",
+    age:24  },
+{
+name:"Soniya",
+age:21 },
+{
+name:"harshita",
+age:21 }]
+
+
+arrayOfObject.forEach( (item)=>{
+    console.log(item.name)
+} )
